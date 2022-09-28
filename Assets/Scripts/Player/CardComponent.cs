266eba8376269f0ae34d71cb88efbe65568ce979
Thomas <thomasciampossin@live.fr>
2022-09-28 16:21:33 +0200

    JT-2: Merged + Spell launching beginning.

--- Assets/Scripts/Player/CardComponent.cs
+++ Assets/Scripts/Player/CardComponent.cs
@@ -5,6 +5,8 @@ using MyBox;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.EventSystems;
+using Jemkont.GridSystem;
+
 public class CardComponent : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler {
 
     public ScriptableCard CardData;
@@ -13,34 +15,43 @@ public class CardComponent : MonoBehaviour, IPointerEnterHandler, IPointerClickH
     public TextMeshPro TitleText;
     public TextMeshPro DescText;
 
+    [ReadOnly] public bool isInPlacingMode;
+
     [ReadOnly] public bool isHovered;
     [ReadOnly] public bool isPressed;
     [ReadOnly] public bool isDragged;
     public float dragSensivity = 60f;
 
     private Vector2 originPosition;
+    [ReadOnly]public Cell HoveredCell;
+
     private void Start() {
         originPosition = transform.position;
     }
     public void Update() {
         Vector2 mousePos = Input.mousePosition;
         if (isDragged) {
-            Drag(mousePos);
+            this.Drag(mousePos);
         }
         if (isPressed) {
             //Check with sensivity:
             var min = this.MinimumDragPosition();
             if (min.x <= mousePos.x || min.y <= mousePos.y) {
-                isDragged = true;
+                this.isDragged = true;
+                CardDraggingSystem.instance.DraggedCard = this;
             }
         }
     }
-    private Vector2 MinimumDragPosition() {
-        //Corresponds to the minimum drag we have to do before the cards begins to get dragged
-        Vector2 pos = this.transform.position;
-        pos.x += dragSensivity;
-        pos.y += dragSensivity;
-        return pos;
+    public void Dissapear() {
+        //Idfk
+        //TODO : MAke card dissapear + FX appears?
+        IllustrationImage.SetAlpha(0);
+        isInPlacingMode = true;
+    }
+    public void ReAppear() {
+        //In case you cancel the spell or anything.
+        IllustrationImage.SetAlpha(1);
+        isInPlacingMode = false;
     }
     public void Hydrate(ScriptableCard CardData) {
         this.CardData = CardData;
@@ -56,35 +67,53 @@ public class CardComponent : MonoBehaviour, IPointerEnterHandler, IPointerClickH
         this.TitleText.text = this.CardData.Title;
         this.DescText.text = this.CardData.Description;
     }
-
+    public void CastSpell(Cell cellToCastSpellOn) {
+        // /!\ position might be off.
+        Instantiate(CardData.Spell,cellToCastSpellOn.WorldPosition,Quaternion.identity);
+        Destroy(gameObject,1.5f);
+    }
+    #region pointerHandlers&Drag
+    private Vector2 MinimumDragPosition() {
+        //Corresponds to the minimum drag we have to do before the cards begins to get dragged
+        Vector2 pos = this.transform.position;
+        pos.x += dragSensivity;
+        pos.y += dragSensivity;
+        return pos;
+    }
     public void OnPointerClick(PointerEventData eventData) {
         //System design
     }
-
     public void OnPointerDown(PointerEventData eventData) {
         if (isPressed || isHovered) {
             //Selected?
             isPressed = true;
         }
     }
-
     public void OnPointerEnter(PointerEventData eventData) {
         isHovered = true;
     }
-
     public void OnPointerExit(PointerEventData eventData) {
         isHovered = false;
     }
-
+    public void OnPointerUp(PointerEventData eventData) {
+        
+        if(isInPlacingMode && HoveredCell != null) {
+            //Needs to check if you can cast the spell on this cell too. If you can:
+            CastSpell(HoveredCell);
+        } else {
+            isPressed = false;
+            isDragged = false;
+            transform.position = originPosition;
+            ReAppear();
+            //Card is going to be reset anyways.
+        }
+    }
     private void Drag(Vector2 mousePos) {
         Vector2 pos = transform.position;
         pos.x = Mathf.Lerp(pos.x,mousePos.x,0.1f);
         pos.y = Mathf.Lerp(pos.y,mousePos.y,0.1f);
         transform.position = pos;
     }
-    public void OnPointerUp(PointerEventData eventData) {
-        isPressed = false;
-        isDragged = false;
-        transform.position = originPosition;
-    }
+    #endregion
+
 }
--- Assets/Scripts/ScriptableObjects/Cards/ScriptableCard.cs
+++ Assets/Scripts/ScriptableObjects/Cards/ScriptableCard.cs
@@ -4,11 +4,12 @@ using System.Threading.Tasks;
 using UnityEngine;
 public enum EChipType { Melee, Ranged, Special }
 [CreateAssetMenu(menuName ="Chip")]
-public class ScriptableCard : ScriptableObject {
+public class  ScriptableCard : ScriptableObject {
     public string Title;
     public int Cost;
-    [TextArea]public string Description;
+    [TextArea] public string Description;
     public Sprite IllustrationImage;
+    public GameObject Spell;
 }
 public class CardComparer : IComparer<ScriptableCard> {
     public int Compare(ScriptableCard x, ScriptableCard y) {
