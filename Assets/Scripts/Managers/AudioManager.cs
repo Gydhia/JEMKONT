31177463d563f7e5f5c2336279a1d4fe55504bee
Old <marco@headbang.club>
2023-02-17 11:42:39 +0100

    created the AudioManager script did not put it in the manager prefab just in case, some missing files i hope i don't break anything

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DownBelow.Managers
+{
+    public class AudioManager : _baseManager<AudioManager>
+    {
+
+
+
+    }
+}
