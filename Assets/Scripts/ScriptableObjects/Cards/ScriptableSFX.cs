2a5f8337768735b286c6364cce912355ec76e515
Thomas <thomasciampossin@live.fr>
2023-04-21 10:28:01 +0200

    JT-58: Moved all methods in the scriptable for now.

--- Assets/Scripts/ScriptableObjects/Cards/ScriptableSFX.cs
+++ Assets/Scripts/ScriptableObjects/Cards/ScriptableSFX.cs
@@ -1,6 +1,12 @@
+using DG.Tweening;
+using DownBelow.Entity;
+using DownBelow.GridSystem;
+using DownBelow.Spells;
+using EODE.Wonderland;
 using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace DownBelow.Mechanics {
@@ -19,6 +25,71 @@ namespace DownBelow.Mechanics {
         public ESFXTravelType TravelType;
         //Sounds too, one day,maybe...
         [EnableIf("@TravelType != ESFXTravelType.Instantaneous")]
-        public float TravelTime = 0.35f;
+        public float TravelDuration = 0.35f;
+
+        async Task ProjectileGoTo(Transform ProjTransform, Transform Target)
+        {
+            ProjTransform.transform.DOLookAt(Target.position, 0f);
+            ProjTransform.transform.DOMoveX(Target.position.x, TravelDuration);
+            ProjTransform.transform.DOMoveZ(Target.position.z, TravelDuration);
+            //TODO : MoveY To have an arching projectile? Also, easings? tweaking? Polishing?
+            await new WaitForSeconds(TravelDuration);
+            ProjTransform.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+        public virtual async Task DoSFX(CharacterEntity caster, Cell target, Spell spell)
+        {
+            GameObject proj = null;
+            Animator anim = null;
+            float landTime;
+            if (anim != null)
+            {
+                landTime = (float)anim.GetStateDuration("Landed");
+            } else
+            {
+                landTime = (float)0f;
+            }
+
+            switch (TravelType)
+                {
+                    case ESFXTravelType.ProjectileToEnemy:
+                        proj = Instantiate(Prefab, caster.transform.position, Quaternion.identity);
+                        anim = proj.GetComponent<Animator>();
+                        //entry state is "MidAir" for now.
+                        await ProjectileGoTo(proj.transform, target.transform);
+                        //Landed!
+                        if (anim != null) anim.SetTrigger("Landed");
+                        Destroy(proj, landTime);
+                        break;
+                    case ESFXTravelType.Instantaneous:
+                        Destroy(Instantiate(Prefab, target.transform.position, Quaternion.identity), 6f);
+                        //TODO: quaternion.LookRotation to target?
+                        break;
+                    case ESFXTravelType.ProjectileFromEnemy:
+                        proj = Instantiate(Prefab, target.transform.position, Quaternion.identity);
+                        anim = proj.GetComponent<Animator>();
+                        //entry state is "MidAir" for now.
+                        await ProjectileGoTo(proj.transform, caster.transform);
+                        //Landed!
+                        if (anim != null) anim.SetTrigger("Landed");
+                        Destroy(proj, landTime);
+                        break;
+                    case ESFXTravelType.ProjectileBackAndForth:
+                        proj = Instantiate(Prefab, caster.transform.position, Quaternion.identity);
+                        anim = proj.GetComponent<Animator>();
+                        //entry state is "MidAir" for now, going for target.
+                        await ProjectileGoTo(proj.transform, target.transform);
+                        //Landed for the first time! Going back.
+                        if (anim != null) anim.SetTrigger("Landed");
+                        await new WaitForSeconds(landTime);
+                        if (anim != null) anim.SetTrigger("TookOff");
+                        //state is "SecondMidAir" now, going for caster.
+                        await ProjectileGoTo(proj.transform, caster.transform);
+                        //Landed again!
+                        if (anim != null) anim.SetTrigger("Landed");
+                        Destroy(proj, landTime);
+                        break;
+                }
+        }
     }
 }
