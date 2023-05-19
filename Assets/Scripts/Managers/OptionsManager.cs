d8ab53329ba8ae6a61c33c570a478b59f0e7101f
Barbabulle <ninosciab@gmail.com>
2023-05-19 16:27:00 +0200

    WIP Main Menu

--- Assets/Scripts/Managers/OptionsManager.cs
+++ Assets/Scripts/Managers/OptionsManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DownBelow.Managers;
@@ -5,5 +6,51 @@ using UnityEngine;
 
 public class OptionsManager : _baseManager<OptionsManager>
 {
+    public void UpdateBrightness(float value)
+    {
+        Screen.brightness = value;
+    }
 
+    public void UpdateDisplayMode(DisplayModes display)
+    {
+        switch (display)
+        {
+              case DisplayModes.FullScreen:
+                  Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                  break;
+              case DisplayModes.Borderless:
+                  Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                  break;
+              case DisplayModes.Windowed:
+                  Screen.fullScreenMode = FullScreenMode.Windowed;
+                  break;
+              default:
+                  Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                  break;
+        }
+        
+        Save();
+    }
+    
+    
+    
+    
+    
+    public void UpdateGameVolume(float value)
+    {
+        
+    }
+    public void UpdateMusicVolume(float value)
+    {
+        
+    }
+    public void UpdateEffectsVolume(float value)
+    {
+        
+    }
+    
+    private void Save()
+    {
+        //TODO Save les settings
+    }
 }
