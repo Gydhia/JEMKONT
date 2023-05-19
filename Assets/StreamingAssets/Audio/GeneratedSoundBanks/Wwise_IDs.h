fa2ded9b17dc0f837b92f2e7a1c74678a9e56134
Old <marco@headbang.club>
2023-05-19 09:08:24 +0200

    added second combat theme and win jingle

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,54 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace STATES
+    {
+        namespace LAYERS
+        {
+            static const AkUniqueID GROUP = 3298531235U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LAYERS0 = 602912169U;
+                static const AkUniqueID LAYERS1 = 602912168U;
+                static const AkUniqueID LAYERS2 = 602912171U;
+                static const AkUniqueID LAYERS3 = 602912170U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace LAYERS
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SB_AMBIANCE = 1129547629U;
+        static const AkUniqueID SB_COMBAT = 2497815141U;
+        static const AkUniqueID SB_FARM = 513513905U;
+        static const AkUniqueID SB_MENU = 135602584U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
