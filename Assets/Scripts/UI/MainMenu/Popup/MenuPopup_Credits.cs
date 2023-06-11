23a87194b5d7fcdafd0fd2b93a3f326867eee2c0
Barbabulle <nino.sciabbarrasi.auditeur@lecnam.net>
2023-06-11 15:41:14 +0200

    JT-90 Began credits

---
+++ Assets/Scripts/UI/MainMenu/Popup/MenuPopup_Credits.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using DownBelow.UI.Menu;
+using UnityEngine;
+
+public class MenuPopup_Credits : BaseMenuPopup
+{
+    [SerializeField] private RectTransform _creditsText;
+}
