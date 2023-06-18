443f5ce4e3c469fd58d2d4d5468f162546cd539a
DonTomasDeLaVega <thomasciampossin@live.fr>
2023-06-18 18:46:30 +0200

    JT-89: SHADOW WIZARD MONEY GANG WE LOOOOOOOOOOVE CASTING SPELLS

--- Assets/Features/Inventory/Scripts/AbyssPreset.cs
+++ Assets/Features/Inventory/Scripts/AbyssPreset.cs
@@ -4,7 +4,6 @@ using DownBelow.Mechanics;
 using Sirenix.OdinInspector;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 namespace DownBelow.Mechanics
 {
